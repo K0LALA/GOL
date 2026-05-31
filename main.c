878e248b417b7df5301b370d408aed8c778adaa8
K0LALA <kolala08@outlook.fr>
2026-05-31 19:50:53 +0200

    Implemented GOL when pressing key

--- main.c
+++ main.c
@@ -1,4 +1,7 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
 
 #define SDL_MAIN_USE_CALLBACKS 1  /* use the callbacks instead of main() */
 #include <SDL3/SDL.h>
@@ -25,9 +28,29 @@ void initSurfaces() {
     SDL_FillSurfaceRect(aliveCell, NULL, 0xFFFFFFFF);
 }
 
+void initCells() {
+    int y;
+    for (y = 0; y < HEIGHT; y++) {
+        int x;
+        for (x = 0; x < WIDTH; x++) {
+            int r = rand();
+            *(*(cells + y) + x) = (r % 13 == 0);
+        }
+    }
+}
+
 /* This function runs once at startup. */
 SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[])
 {
+    /* Initialize random */
+    srand((int)time(NULL));
+
+    /* Initialize SDL */
+    if (!SDL_Init(SDL_INIT_VIDEO)) {
+        SDL_Log("Couldn't initialize SDL: %s\n", SDL_GetError());
+        return SDL_APP_FAILURE;
+    }
+
     /* Create the window */
     if (!SDL_CreateWindowAndRenderer("SDL3 Draw", 800, 600, SDL_WINDOW_RESIZABLE, &window, &renderer)) {
         SDL_Log("Couldn't create window and renderer: %s\n", SDL_GetError());
@@ -46,6 +69,8 @@ SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[])
     deadCell = SDL_CreateSurface(PIXEL_SIZE, PIXEL_SIZE, SDL_PIXELFORMAT_RGBA32);
     aliveCell = SDL_CreateSurface(PIXEL_SIZE, PIXEL_SIZE, SDL_PIXELFORMAT_RGBA32);
     initSurfaces();
+
+    initCells();
 
     return SDL_APP_CONTINUE;
 }
@@ -66,8 +91,35 @@ void displayCells() {
     SDL_RenderPresent(renderer);
 }
 
-int i = 0;
-int changed = 1;
+void nextStep() {
+    uint8_t *line = (uint8_t*)malloc(WIDTH);
+    int y;
+    for (y = 0; y < HEIGHT; y++) {
+        int x;
+        for (x = 0; x < WIDTH; x++) {
+            uint8_t cellCount = 0;
+            int dx;
+            for (dx = -1; dx <= 1; dx++) {
+                int dy;
+                for (dy = -1; dy <= 1; dy++) {
+                    if (cellCount > 3) break;
+                    if ((dx == 0 && dy == 0) ||
+                            x + dx < 0 || x + dx >= WIDTH ||
+                            y + dy < 0 || y + dy >= HEIGHT) continue;
+                    if (*(*(cells + y + dy) + x + dx)) cellCount++;
+                }
+            }
+            if (*(*(cells + y) + x) && cellCount == 2)
+                *(line + x) = 1;
+            else if (cellCount == 3)
+                *(line + x) = 1;
+            else
+                *(line + x) = 0;
+        }
+        memcpy(*(cells + y), line, WIDTH);
+    }
+    free(line);
+}
 
 /* This function runs when a new event (mouse input, keypresses, etc) occurs. */
 SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event)
@@ -79,20 +131,15 @@ SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event)
         if (event->key.key == SDLK_ESCAPE) {
             return SDL_APP_SUCCESS;
         }
-        *(*(cells + i % WIDTH) + (int)(i / HEIGHT)) = 1;
-        i++;
-        changed=1;
+        nextStep();
     }
     return SDL_APP_CONTINUE;
 }
 
 /* This function runs once per frame, and is the heart of the program. */
 SDL_AppResult SDL_AppIterate(void *appstate)
-{
-    if (changed) {
-        changed = 0;
-        displayCells();
-    }
+{   
+    displayCells();
 
     return SDL_APP_CONTINUE;
 }
@@ -100,4 +147,7 @@ SDL_AppResult SDL_AppIterate(void *appstate)
 /* This function runs once at shutdown. */
 void SDL_AppQuit(void *appstate, SDL_AppResult result)
 {
+    SDL_DestroyTexture(cellsTexture);
+    SDL_DestroySurface(aliveCell);
+    SDL_DestroySurface(deadCell);
 }
