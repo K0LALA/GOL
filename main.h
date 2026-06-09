3e80b04e1214d2f35c3ca3638c6fcf46de7cd64f
K0LALA <kolala08@outlook.fr>
2026-06-09 14:42:33 +0200

    Fixed prototype not matching function in main.h

--- main.h
+++ main.h
@@ -4,7 +4,7 @@
 #include "cells.h"
 
 void addCell(int16_t x, int16_t y);
-void initCells();
+void initCells(unsigned int w, unsigned int h);
 void initPattern(char* fileName, int offsetX, int offsetY);
 void displayCells();
 void nextStep();
